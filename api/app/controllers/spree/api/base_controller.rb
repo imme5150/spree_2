cf0326c9f856b694297fe653384695d2c243c9a4
Josh Shupack <ynaught@gmail.com>
2025-06-09 21:14:19 -0700

    remove SSL monitoring, now configured by rails by default

--- api/app/controllers/spree/api/base_controller.rb
+++ api/app/controllers/spree/api/base_controller.rb
@@ -4,7 +4,6 @@ module Spree
   module Api
     class BaseController < ActionController::Base
       include Spree::Api::ControllerSetup
-      include Spree::Core::ControllerHelpers::SSL
       include Spree::Core::ControllerHelpers::StrongParameters
 
       attr_accessor :current_api_user
@@ -20,8 +19,6 @@ module Spree
       rescue_from ActiveRecord::RecordNotFound, :with => :not_found
 
       helper Spree::Api::ApiHelpers
-
-      ssl_allowed
 
       def set_jsonp_format
         if params[:callback] && request.get?
--- frontend/app/controllers/spree/checkout_controller.rb
+++ frontend/app/controllers/spree/checkout_controller.rb
@@ -4,8 +4,6 @@ module Spree
   # checkout which has nothing to do with updating an order that this approach
   # is waranted.
   class CheckoutController < Spree::StoreController
-    ssl_required
-
     before_action :load_order_with_lock
 
     before_action :ensure_order_not_completed
