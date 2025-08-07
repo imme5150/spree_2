69f76ec53fa9136ba1200992157f753fcfe2141f
Josh Shupack <ynaught@gmail.com>
2025-08-06 17:54:01 -0700

    remove respond_with helper

--- core/lib/spree/core/controller_helpers.rb
+++ core/lib/spree/core/controller_helpers.rb
@@ -1,6 +1,5 @@
 require 'spree/core/controller_helpers/common'
 require 'spree/core/controller_helpers/auth'
-require 'spree/core/controller_helpers/respond_with'
 require 'spree/core/controller_helpers/order'
 
 module Spree
@@ -10,7 +9,6 @@ module Spree
         klass.class_eval do
           include Spree::Core::ControllerHelpers::Common
           include Spree::Core::ControllerHelpers::Auth
-          include Spree::Core::ControllerHelpers::RespondWith
           include Spree::Core::ControllerHelpers::Order
         end
       end
