ba2feb7fc644ef9fc3d637e5006a789a04dbc952
Josh Shupack <ynaught@gmail.com>
2025-08-25 12:50:59 -0700

    remove paperclip gem

--- core/app/models/spree/taxon.rb
+++ core/app/models/spree/taxon.rb
@@ -12,13 +12,6 @@ module Spree
 
     after_touch :touch_parent
 
-    has_attached_file :icon,
-      styles: { mini: '32x32>', normal: '128x128>' },
-      default_style: :mini,
-      url: '/spree/taxons/:id/:style/:basename.:extension',
-      path: ':rails_root/public/spree/taxons/:id/:style/:basename.:extension',
-      default_url: '/assets/default_taxon.png'
-
     include Spree::Core::ProductFilters  # for detailed defs of filters
 
     # indicate which filters should be used for a taxon
